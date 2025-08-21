c765109e63794b1eae74838c1091bb796e580e41
Raghu Betina <Raghu.Betina@chicagobooth.edu>
2025-08-21 01:03:34 +0000

    Generated Board resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
 Rails.application.routes.draw do
+  # Routes for the Board resource:
+
+  # CREATE
+  post("/insert_board", { :controller => "boards", :action => "create" })
+
+  # READ
+  get("/boards", { :controller => "boards", :action => "index" })
+
+  get("/boards/:path_id", { :controller => "boards", :action => "show" })
+
+  # UPDATE
+
+  post("/modify_board/:path_id", { :controller => "boards", :action => "update" })
+
+  # DELETE
+  get("/delete_board/:path_id", { :controller => "boards", :action => "destroy" })
+
+  #------------------------------
+
   # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
   # get("/your_first_screen", { :controller => "pages", :action => "first" })
 end
